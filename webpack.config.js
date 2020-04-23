15d1ec743ab77a1abb7ddde6db46b5a89a4ec5f2
andrew <andrewfldn@gmail.com>
2020-04-23 20:33:44 +0100

    Webpack and React setup

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,12 @@ module.exports = {
     bundle: glob.sync("./angular-application/**/*.js")
 },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'stage-0', 'react'],
+        use: {
+          loader: 'babel-loader'
         }
       },
       {
